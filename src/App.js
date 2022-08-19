83176828de478ca362a311e384de9eacacad0101
Kawul007 <1694436003@qq.com>
2022-08-19 14:36:13 +0800

    更改图片位置

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Pic from "../src/images/img-2.jpg";
 function App() {
   return (
     <div className="App">
       <h1>今天要完成</h1>
       <h2>这次要成功啊！！！！！！！！！！！</h2>
-      <img src='../../images/img-2.jpg' style={{width:500,height:500}}></img>
+      <img src={Pic} style={{width:500,height:500}}></img>
     </div>
   );
 }

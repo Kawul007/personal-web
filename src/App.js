f31e11ae7341a5b24cb005242fe77ddba207c65c
Kawul007 <1694436003@qq.com>
2022-08-19 16:01:08 +0800

    图片

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import Pic from "../src/images/img-2.jpg";
+import Pic from "../src/images/test.jpg"
 function App() {
   return (
     <div className="App">
-      <h1>今天要完成</h1>
       <h2>这次要成功啊！！！！！！！！！！！</h2>
-      <img src={Pic} style={{width:500,height:500}}></img>
+      <img src={Pic} alt="图片无法显示" style={{width:500,height:500}}></img>
     </div>
   );
 }

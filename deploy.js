40832fe57febb02cf0ee3bb4371d5c02ccaae789
Kawul007 <1694436003@qq.com>
2022-09-12 22:18:02 +0800

    解决github-pages报错404问题

--- deploy.js
+++ deploy.js
@@ -7,9 +7,10 @@ const routes = [
  
 ]
 
+
 // 给每个route都新建一个文件夹，把index.html拷贝进去
-import { copySync } from 'fs-extra'
-import { join } from 'path'
+const fs = require('fs-extra')
+const path = require('path')
 routes.forEach((route) => {
-    copySync(join('build', 'index.html'), join('build', route, 'index.html'))
+    fs.copySync(path.join('build', 'index.html'), path.join('build', route, 'index.html'))
 })

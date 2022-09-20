069ac4448b85b0c66a7672b16869805717d3993b
Kawul007 <1694436003@qq.com>
2022-09-20 20:55:05 +0800

    detailsPage修正

--- src/components/BugDetails/BugDetails.style.js
+++ src/components/BugDetails/BugDetails.style.js
@@ -8,14 +8,18 @@ export const BugDetailImage = styled.div`
   display: inline-block;
   width: 100%;
   height: 300px;
+  position: absolute;
   /* overflow: hidden; */
   img {
     display: inline-block;
     /* overflow: hidden; */
+    position: relative;
     width: 100%;
     height: 100%;
+    margin-top: -50px;
     background-size: cover;
     vertical-align: top;
+    z-index: -1;
   }
   .demo {
     position: absolute;
@@ -37,6 +41,19 @@ export const BugDetailImage = styled.div`
       }
     }
   }
+  .back {
+    position: absolute;
+    width: 100%;
+    height: 50px;
+    z-index: 111111;
+    a {
+      margin: 5px 50px;
+      text-decoration: none;
+      color: white;
+      font-weight: 700;
+      font-size: 22px;
+    }
+  }
 `;
 export const BugImageInfo = styled.div`
   position: absolute;
@@ -55,34 +72,45 @@ export const BugDetailContent = styled.div`
   float: left;
   width: 80%;
   height: auto;
-
-  background-color: aqua;
+  margin: 15px 0px;
+  border-radius: 10px;
+  background-color: #c4fbec;
   #intro {
     margin: 0;
     padding: 0;
     width: 100%;
-    height: 100vh;
-    background-color: blue;
+    height: auto;
+    border-radius: 10px 10px 0px 0px;
+    /* box-shadow: 5px 0px 5px 5px grey; */
   }
   #logic {
     width: 100%;
     height: 100vh;
-    background-color: violet;
+    /* box-shadow: 5px 5px 5px grey; */
   }
   #process {
     width: 100%;
     height: 100vh;
-    background-color: green;
+    /* box-shadow: 0px 0px 5px grey; */
   }
   #all {
     width: 100%;
     height: 100vh;
-    background-color: greenyellow;
+    border-radius: 0px 0px 10px 10px;
+    /* box-shadow: 5px 5px 5px grey; */
   }
-  h4 {
+  h3 {
+    width: 90%;
+    color: #000;
+    font-weight: 700;
     padding: 30px;
     margin-block-start: 0;
     margin-block-end: 0;
+    border-bottom: 1px solid #000;
+  }
+  p {
+    padding: 10px;
+    color: #000;
   }
 `;
 
@@ -93,22 +121,93 @@ export const BugDetailRecomend = styled.div`
   height: 100vh;
   top: 0px;
   left: 0;
-  background-color: #d7d7d7;
+  /* background-color: #f0bdef; */
+`;
+export const BugDetailCategroy = styled.div`
+  /* padding: 20px;
+box-sizing: border-box; */
+  margin: 15px 5%;
+  width: 90%;
+  height: 200px;
+  border-radius: 10px;
+  background-color: #c4fbec;
+  box-shadow: 5px 5px 5px grey;
   h3 {
+    padding: 5px;
     width: fit-content;
     color: #000;
     font-weight: 700;
     padding-left: 20px;
+    margin-block-start: 0;
+    margin-block-end: 0;
+  }
+  h3 a {
+    text-decoration: none;
+    color: #000;
   }
   ul li {
-    width: fit-content;
+    margin-top: 5px;
+    width: 70%;
     padding-left: 20px;
+
     font-size: 18px;
     list-style: none;
+    &:hover {
+      background-color: #f0bdef;
+    }
   }
   ul li a {
+    width: 70%;
     text-decoration: none;
     color: #000;
     font-weight: 700;
+    &:active {
+      color: #f0bdef;
+    }
+  }
+`;
+export const BugDetailMore = styled.div`
+  height: 500px;
+  margin: 15px 5%;
+  width: 90%;
+  height: 400px;
+  border-radius: 10px;
+  background-color: #c4fbec;
+  box-shadow: 5px 5px 5px grey;
+  h3 {
+    color: #000;
+    padding: 10px;
+    margin-left: 10px;
+  }
+  ul {
+    margin-block-start: 0em;
+    margin-block-end: 0em;
+  }
+  ul li {
+    width: 90%;
+    height: 60px;
+    margin: 5px 5%;
+    /* background: #000; */
+  }
+  ul li a img {
+    float: left;
+    margin: 5px 0px;
+    width: 35%;
+    height: 50px;
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+  ul li a div {
+    float: left;
+    margin-left: 8px;
+  }
+  ul li a div p {
+    color: #000;
+    font-weight: 500;
+    padding: 5px;
+    font-size: 12px;
+    margin-block-start: 0em;
+    margin-block-end: 0em;
   }
 `;
--- src/components/BugDetails/index.js
+++ src/components/BugDetails/index.js
@@ -5,16 +5,26 @@ import {
   BugDetailImage,
   BugImageInfo,
   BugDetailRecomend,
+  BugDetailCategroy,
+  BugDetailMore,
 } from "./BugDetails.style";
 import FloatBtn from "../FloatBtn/index";
 import Img from "../../images/blog-img14.jpg";
+import Img01 from "../../images/blog-img28.jpg";
+import Img02 from "../../images/blog-img29.jpg";
+import Img03 from "../../images/blog-img23.png";
+import Img04 from "../../images/blog-img24.png";
+import Img05 from "../../images/blog-img22.png";
 import "../../iconfont.css";
 const BugDetails = () => {
   return (
     <BugDetailWrapper>
       <BugDetailImage>
+        <div className="back">
+          <a href="/personal-web/blogPage">Kai4234</a>
+        </div>
         <img src={Img}></img>
-        <div className="demo">
+        <div className="demo" id="title">
           <h2>本网站技术总结</h2>
           <p>
             <span>
@@ -58,38 +68,101 @@ const BugDetails = () => {
       <BugImageInfo>
         <BugDetailContent>
           <div id="intro">
-            <h4>项目概述</h4>
+            <h3>1.项目概述</h3>
+            <p>
+              (1)&nbsp;本站主要是用来记录个人学习前后端以及语言相关的知识，主要使用技术有：
+            </p>
+            <p>(2)&nbsp;前端React、Js、HTML、CSS等；</p>
+            <p>(3)&nbsp;后端SpringBoot、Mybatis-Plus、MYSQL编写的调试过程.</p>
           </div>
           <div id="logic">
-            <h4>实现逻辑</h4>
+            <h3>2.实现逻辑</h3>
           </div>
           <div id="process">
-            <h4>实现过程</h4>
+            <h3>3.实现过程</h3>
           </div>
           <div id="all">
-            <h4>综述</h4>
+            <h3>4.综述</h3>
           </div>
         </BugDetailContent>
         <BugDetailRecomend>
-          <h3>
+          <BugDetailCategroy>
             {" "}
-            <i className="iconfont icon-category"></i>
-            &nbsp;目录
-          </h3>
-          <ul>
-            <li>
-              <a href="#intro">1.项目概述</a>
-            </li>
-            <li>
-              <a href="#logic">2.实现逻辑</a>
-            </li>
-            <li>
-              <a href="#process">3.实现过程</a>
-            </li>
-            <li>
-              <a href="#all">4.综述</a>
-            </li>
-          </ul>
+            <h3>
+              <a href="#title">
+                <i className="iconfont icon-category"></i>
+                &nbsp;目录
+              </a>
+            </h3>
+            <ul>
+              <li>
+                <a href="#intro">1.项目概述</a>
+              </li>
+              <li>
+                <a href="#logic">2.实现逻辑</a>
+              </li>
+              <li>
+                <a href="#process">3.实现过程</a>
+              </li>
+              <li>
+                <a href="#all">4.综述</a>
+              </li>
+            </ul>
+          </BugDetailCategroy>
+          <BugDetailMore>
+            <h3>
+              {" "}
+              <i className="iconfont icon-shijian"></i>
+              &nbsp;更多内容
+            </h3>
+            <ul>
+              <li>
+                <a>
+                  <img src={Img01}></img>
+                  <div>
+                    <p>我的2022</p>
+                    <p>2022-12-31</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <img src={Img02}></img>
+                  <div>
+                    <p>CSS三大布局</p>
+                    <p>2022-09-20</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <img src={Img03}></img>
+                  <div>
+                    <p>SpringBoot</p>
+                    <p>2022-11-31</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <img src={Img04}></img>
+                  <div>
+                    <p>我的大学</p>
+                    <p>2018-2022</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <img src={Img05}></img>
+                  <div>
+                    <p>MySQL相关</p>
+                    <p>2022-10-31</p>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </BugDetailMore>
         </BugDetailRecomend>
       </BugImageInfo>
       <FloatBtn />

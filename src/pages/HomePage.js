87224247714ae132d74a8f191b0f990440f133c1
guantube <kai423455@gmail.com>
2022-10-11 23:19:01 +0800

    homepage调整

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -24,7 +24,7 @@ const HomePage = () => {
       <NavBar toggle={toggle} />
       <ActiveSection />
      
-      <BlogSection />
+      {/* <BlogSection /> */}
       <MainSection />
 
       <Games />
